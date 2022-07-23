50500f5162ea695b4dada8550f57d9419269a882
OrangeBai <jzp0306@gmail.com>
2022-07-23 19:07:02 +0100

    .

--- exps/compute_jac.py
+++ exps/compute_jac.py
@@ -20,7 +20,8 @@ def compute_jac_cnn(net, img, activation='ReLU'):
         if type(module) == LinearBlock:
             x = module.BN(module.FC(x))
             pattern = get_pattern(x, Gamma=set_gamma(activation))
-            _, ub = layer_lb_ub(pattern, grad_bound=[(0, 0), (1, 1)])
+            _, ub = layer_lb_ub(pattern, grad_bound=set_lb_ub(activation))
+            x = module.Act(x)
 
             batch_x = fc(batch_x, module.FC)
             batch_x = batch_norm(batch_x, module.BN)
@@ -30,6 +31,7 @@ def compute_jac_cnn(net, img, activation='ReLU'):
             x = module.BN(module.Conv(x))
             pattern = get_pattern(x, Gamma=set_gamma(activation))
             _, ub = layer_lb_ub(pattern, grad_bound=[(0, 0), (1, 1)])
+            x = module.Act(x)
 
             batch_x = conv(batch_x, module.Conv)
             batch_x = batch_norm(batch_x, module.BN)
@@ -37,9 +39,6 @@ def compute_jac_cnn(net, img, activation='ReLU'):
                 batch_x = act_pattern(batch_x, ub)
         elif type(module) == nn.MaxPool2d:
             x2, indices = F.max_pool2d(x, module.kernel_size, module.stride, return_indices=True)
-
-            flattened_tensor = x.flatten(start_dim=2)
-            output = flattened_tensor.gather(dim=2, index=indices.flatten(start_dim=2)).view_as(indices)
 
             indices = torch.concat([indices] * len(batch_x))
             flattened_tensor = batch_x.flatten(start_dim=2)
@@ -53,12 +52,12 @@ def compute_jac_cnn(net, img, activation='ReLU'):
     t2 = time.time()
     print(t2 - t1)
     img.requires_grad = True
-    # y = net(img)[0]
-    # g = []
-    # for i in range(len(y)):
-    #     g += [torch.autograd.grad(y[i], img, retain_graph=True)[0]]
+    y = net(img)[0]
+    g = []
+    for i in range(len(y)):
+        g += [torch.autograd.grad(y[i], img, retain_graph=True)[0]]
 
-    # g = torch.concat(g)
+    g = torch.concat(g)
     j = torch.autograd.functional.jacobian(net, images)
     print(time.time() - t2)
     permuted = j.squeeze().permute(1, 2, 3, 0)
@@ -90,14 +89,14 @@ def conv(x, conv_layer):
 if __name__ == '__main__':
     kwargs = {'activation': 'ReLU', 'batch_norm': 1}
     layers = [
-        ConvBlock(3, 32, 3, batch_norm=1, padding=1, stride=2,  activation='ReLU'),
+        ConvBlock(3, 32, 3, batch_norm=1, padding=1, stride=2, activation='ReLU'),
         ConvBlock(32, 32, 3, padding=1, stride=1, activation='ReLU', batch_norm=1),
-    #
-              # ConvBlock(16, 16, batch_norm=True, padding=1, stride=2, activation='ReLU'),
-              nn.MaxPool2d(kernel_size=2, stride=2),
-              nn.Flatten(),
-              LinearBlock(2048, 10, batch_norm=0, activation=None)
-              ]
+        #
+        # ConvBlock(16, 16, batch_norm=True, padding=1, stride=2, activation='ReLU'),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        LinearBlock(2048, 10, batch_norm=0, activation=None)
+    ]
     model = nn.Sequential(*layers)
     # model = nn.Sequential(
     #     ConvBlock(3, 32, 3, padding=1, **kwargs),
@@ -117,4 +116,3 @@ if __name__ == '__main__':
     images = torch.randn((1, 3, 32, 32))
     # images = torch.randn(1, 1, 1, 4096)
     compute_jac_cnn(model, images)
-
