01d9067e7e95759a12623d078f3449530e8ef066
OrangeBai <jzp0306@gmail.com>
2022-09-12 18:46:31 +0800

    .

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from engine import *
 from settings.train_settings import *
 
+
+
 if __name__ == '__main__':
     arg_parser = ArgParser(True)
     for file_path in arg_parser.files:
@@ -8,3 +10,5 @@ if __name__ == '__main__':
 
         trainer = Trainer(arg_parser.get_args())
         trainer.train_model()
+
+
